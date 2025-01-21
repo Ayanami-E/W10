a7655d415b847c0e77e2497cf47fbf907bca5870
Ayanami-E <150331674+Ayanami-E@users.noreply.github.com>
2025-01-21 23:53:31 +0200

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,8 +9,8 @@ const Header = () => {
       <h1>Lorem Ipsum</h1>
       <nav>
         <ul>
-          <li><Link to="/">{t('Home')}</Link></li>
-          <li><Link to="/about">{t('About')}</Link></li>
+          <li><Link to="/">{t('home')}</Link></li>
+          <li><Link to="/about">{t('about')}</Link></li>
           <li><button id="fi" onClick={() => i18n.changeLanguage('fi')}>FI</button></li>
           <li><button id="en" onClick={() => i18n.changeLanguage('en')}>EN</button></li>
         </ul>
