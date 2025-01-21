4534b35769de0d548ea02f4a71a5ed70b401940c
Ayanami-E <150331674+Ayanami-E@users.noreply.github.com>
2025-01-21 23:25:52 +0200

    Update MyContainer.tsx

--- src/components/MyContainer.tsx
+++ src/components/MyContainer.tsx
@@ -1,7 +1,11 @@
+import { useTranslation } from "react-i18next";
+
 const MyContainer = () => {
+  const { t } = useTranslation();
+  
   return (
     <div>
-      <h2>This is the front page.</h2>
+      <h2>{t('This is the front page')}</h2>
     </div>
   );
 };
