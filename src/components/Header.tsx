3c53e059b992ef87d1098c1a859733a310dc3209
Ayanami-E <150331674+Ayanami-E@users.noreply.github.com>
2025-01-22 00:39:33 +0200

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
-import './About.css';
+import './header.css';
 
 const Header = () => {
   const { t, i18n } = useTranslation();
