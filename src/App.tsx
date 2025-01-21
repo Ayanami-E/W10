78ce4272ae2d4b9602a02b30d2393ab3774a4b89
Ayanami-E <150331674+Ayanami-E@users.noreply.github.com>
2025-01-21 23:35:33 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,16 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import MyContainer from './components/MyContainer';
 import About from './components/About';
-import './i18n';
 
 const App = () => {
   return (
     <Router>
       <div>
-        <div data-testid="pg-header">
-          <Header />
-        </div>
-        <main className="container">
+        <Header />
+        <main>
           <Routes>
-            <Route 
-              path="/" 
-              element={<div data-testid="pg-container"><MyContainer /></div>} 
-            />
-            <Route 
-              path="/about" 
-              element={<div data-testid="pg-about"><About /></div>} 
-            />
+            <Route path="/" element={<MyContainer />} />
+            <Route path="/about" element={<About />} />
           </Routes>
         </main>
       </div>
