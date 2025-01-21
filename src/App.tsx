ccf1de6872ebf575364d4e2b9e6841ebdb00f8f2
Ayanami-E <150331674+Ayanami-E@users.noreply.github.com>
2025-01-22 00:43:01 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,21 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import MyContainer from './components/MyContainer';
 import About from './components/About';
-import { Suspense } from 'react';
 import './i18n';
 
 const App = () => {
   return (
-    <Suspense fallback="loading...">
-      <Router>
-        <div>
-          <Header />
-          <main>
-            <Routes>
-              <Route path="/" element={<MyContainer />} />
-              <Route path="/about" element={<About />} />
-            </Routes>
-          </main>
-        </div>
-      </Router>
-    </Suspense>
+    <Router>
+      <div>
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<MyContainer />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 };
 
