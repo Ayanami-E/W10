b5aeb12397b66a99e6613ea3676cca163e769be4
Ayanami-E <150331674+Ayanami-E@users.noreply.github.com>
2025-01-22 00:56:42 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,20 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import MyContainer from './components/MyContainer';
-import About from './components/About';
-import './i18n';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import MyContainer from './components/MyContainer'
+import About from './components/About'
 
 const App = () => {
   return (
     <Router>
-      <div>
-        <Header />
-        <main>
-          <Routes>
-            <Route path="/" element={<MyContainer />} />
-            <Route path="/about" element={<About />} />
-          </Routes>
-        </main>
-      </div>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<MyContainer />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </main>
     </Router>
-  );
-};
+  )
+}
 
-export default App;
+export default App
