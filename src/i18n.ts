2516e4a9267e8c4af77599dfd45800d35a20881f
Ayanami-E <150331674+Ayanami-E@users.noreply.github.com>
2025-01-21 23:46:21 +0200

    Update i18n.ts

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,31 +1,38 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
+import Backend from 'i18next-http-backend';
+
+const resources = {
+  en: {
+    translation: {
+      "Home": "Home",
+      "About": "About",
+      "This is the front page": "This is the front page"
+    }
+  },
+  fi: {
+    translation: {
+      "Home": "Etusivu",
+      "About": "Tietoa Meistä",
+      "This is the front page": "Tämä on etusivu"
+    }
+  }
+};
 
 i18n
+  .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
+    resources,
     fallbackLng: 'en',
     debug: true,
     interpolation: {
       escapeValue: false,
     },
-    resources: {
-      en: {
-        translation: {
-          "Home": "Home",
-          "About": "About",
-          "This is the front page": "This is the front page"
-        }
-      },
-      fi: {
-        translation: {
-          "Home": "Etusivu",
-          "About": "Tietoa Meistä",
-          "This is the front page": "Tämä on etusivu"
-        }
-      }
+    detection: {
+      order: ['navigator']
     }
   });
 
