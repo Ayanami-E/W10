abb57126f35559f328edd175865d4d9d6b92ea59
Ayanami-E <150331674+Ayanami-E@users.noreply.github.com>
2025-01-22 00:48:14 +0200

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,51 @@
 import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
-import './header.css';
 
 const Header = () => {
   const { t, i18n } = useTranslation();
 
   return (
-    <header className="header" data-testid="pg-header">
-      <h1>Lorem Ipsum</h1>
+    <header 
+      className="bg-gray-800 text-white p-4 flex justify-between items-center"
+      data-testid="pg-header"
+    >
+      <h1 className="text-xl font-bold">Lorem Ipsum</h1>
       <nav>
-        <ul>
-          <li><Link to="/">{t('Home')}</Link></li>
-          <li><Link to="/about">{t('About')}</Link></li>
-          <li><button id="fi" onClick={() => i18n.changeLanguage('fi')}>FI</button></li>
-          <li><button id="en" onClick={() => i18n.changeLanguage('en')}>EN</button></li>
+        <ul className="flex gap-4 items-center">
+          <li>
+            <Link 
+              to="/" 
+              className="text-white hover:underline"
+            >
+              {t('Home')}
+            </Link>
+          </li>
+          <li>
+            <Link 
+              to="/about" 
+              className="text-white hover:underline"
+            >
+              {t('About')}
+            </Link>
+          </li>
+          <li>
+            <button 
+              id="fi" 
+              onClick={() => i18n.changeLanguage('fi')}
+              className="text-white hover:underline text-base bg-transparent border-none cursor-pointer"
+            >
+              FI
+            </button>
+          </li>
+          <li>
+            <button 
+              id="en" 
+              onClick={() => i18n.changeLanguage('en')}
+              className="text-white hover:underline text-base bg-transparent border-none cursor-pointer"
+            >
+              EN
+            </button>
+          </li>
         </ul>
       </nav>
     </header>
