fac2d041e2c4202260794c4e298a94c6d7a3cc3d
Ayanami-E <150331674+Ayanami-E@users.noreply.github.com>
2025-01-22 00:23:31 +0200

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
-import '../style/About.css';
+import '../style/header.css';
+import '../style/About.css'; // 保持原来的样式引入
 
 interface Post {
   id: number;
