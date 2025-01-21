a420cb2047e20ef5c6b7a60225322b2d7942d4c2
Ayanami-E <150331674+Ayanami-E@users.noreply.github.com>
2025-01-21 23:47:05 +0200

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,10 +4,6 @@ import { useTranslation } from "react-i18next";
 const Header = () => {
   const { t, i18n } = useTranslation();
 
-  const changeLanguage = (lng: string) => {
-    i18n.changeLanguage(lng);
-  };
-
   return (
     <header className="header" data-testid="pg-header">
       <h1>Lorem Ipsum</h1>
@@ -15,8 +11,8 @@ const Header = () => {
         <ul>
           <li><Link to="/">{t('Home')}</Link></li>
           <li><Link to="/about">{t('About')}</Link></li>
-          <li><button id="fi" onClick={() => changeLanguage('fi')}>FI</button></li>
-          <li><button id="en" onClick={() => changeLanguage('en')}>EN</button></li>
+          <li><button id="fi" onClick={() => i18n.changeLanguage('fi')}>FI</button></li>
+          <li><button id="en" onClick={() => i18n.changeLanguage('en')}>EN</button></li>
         </ul>
       </nav>
     </header>
