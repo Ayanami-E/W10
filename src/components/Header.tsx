aa56ddcb4d0ff6d60d46018d138e369b9ba14fbe
Ayanami-E <150331674+Ayanami-E@users.noreply.github.com>
2025-01-21 23:35:51 +0200

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,15 @@
 import { Link } from "react-router-dom";
-import { useTranslation } from "react-i18next";
 
 const Header = () => {
-  const { t, i18n } = useTranslation();
-
-  const changeLanguage = (lng: string) => {
-    i18n.changeLanguage(lng);
-  };
-
   return (
-    <header className="header">
-      <h1>{t('My Website')}</h1>
+    <header className="header" data-testid="pg-header">
+      <h1>Lorem Ipsum</h1>
       <nav>
         <ul>
-          <li><Link to="/">{t('Home')}</Link></li>
-          <li><Link to="/about">{t('About')}</Link></li>
-          <li><button id="fi" onClick={() => changeLanguage('fi')}>FI</button></li>
-          <li><button id="en" onClick={() => changeLanguage('en')}>EN</button></li>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><button id="fi">FI</button></li>
+          <li><button id="en">EN</button></li>
         </ul>
       </nav>
     </header>
